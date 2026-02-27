62b250e88521badccf25e2922d89a19b1453d7b9
Tanaka Mkaronda <43981156+Tanks1738@users.noreply.github.com>
2026-02-27 22:56:36 +0200

    Remove SSL

--- src/db.js
+++ src/db.js
@@ -2,7 +2,8 @@ const mysql = require("mysql2/promise");
 const fs = require("fs"); // <-- you must import fs to use readFileSync
 require("dotenv").config();
 
-
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "Loaded" : "Missing");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -11,12 +12,13 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 
-  ssl: {
+ /* ssl: {
     // This enforces SSL and rejects invalid certificates
     rejectUnauthorized: true,
      ca: process.env.DB_CA_CERT,
    //  ca: fs.readFileSync("ca.pem"), // path to the downloaded CA cert
   },
+  */
 });
 
 module.exports = pool;
